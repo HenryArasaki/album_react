9c09ab52a7f575fe36844492c583083fb58972b8
Henry Arasaki <henry_arasaki@hotmail.com>
2022-11-09 00:11:47 -0300

    corrigindo bugs no index do album

--- src/pages/Albums.jsx
+++ src/pages/Albums.jsx
@@ -1,11 +1,32 @@
-import Button from "../components/Button"
+import { useState, useEffect } from "react";
 import { useAuth } from "../hooks/auth";
+import Button from "../components/Button";
+import { api } from "../service/api";
 
-export default function Albums(){
-    const {signOut} = useAuth()
+export default function Albums() {
+  const [albums, setAlbums] = useState([]);
+  const { signOut, user } = useAuth();
+  const { name } = user;
 
-    return(<div className="bg-blue-50 h-screen">
-        <h1>Albums</h1>
-        <Button onClick={signOut}>SignOut</Button>
-        </div>)
+  useEffect(() => {
+    async function fetchAlbums() {
+      const response = await api.get("/albums");
+      setAlbums(response.data);
+    }
+    fetchAlbums();
+  });
+
+  return (
+    <div className="bg-blue-50 h-screen">
+      <h1>Albums</h1>
+      <p>Olá, {name}</p>
+      <ul>
+        {albums &&
+          albums.map((album) => {
+            return <li key={album.id}>{album.name}</li>;
+          })}
+      </ul>
+      <Button onClick={signOut}>SignOut</Button>
+    </div>
+  );
 }
