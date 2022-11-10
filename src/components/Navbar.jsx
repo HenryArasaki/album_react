b8c3ac7990ab3fabf99af99f170e77c56604215a
Henry Koiti Arasaki <82346140+HenryArasaki@users.noreply.github.com>
2022-11-10 18:20:41 -0300

    mini atualização no trab

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,13 @@
+import {Link} from 'react-router-dom'
 
 export default function Navbar(){
     return (
         <div className="bg-indigo-200">
             <h1 className="w-full">Albumzinho brabo</h1>
+            <nav>
+                <Link to="/">Albums</Link>
+
+            </nav>
         </div>
     )
 }
--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -21,7 +21,7 @@ function AuthProvider({ children }) {
       if (error.response) {
         alert(error.response.data.message);
       } else {
-        alert("Não fio possível entrar.");
+        alert("Não foi possível entrar.");
       }
     }
   }
