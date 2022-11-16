a14f89edac173e4b9d66f199bfc77e7e4eaae990
Henry Koiti Arasaki <82346140+HenryArasaki@users.noreply.github.com>
2022-11-16 16:03:39 -0300

    multipart para base64

--- src/components/NewPageModal.jsx
+++ src/components/NewPageModal.jsx
@@ -2,37 +2,54 @@ import { useState } from "react";
 import Button from "./Button";
 import { api } from "../service/api";
 
-
 // https://www.freecodecamp.org/news/how-to-build-a-modal-with-javascript/
 export default function NewPageModal(props) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [date, setDate] = useState("");
-  const [photo, setPhoto] = useState(null);
+  const [photo, setPhoto] = useState("");
 
-  const visible = props.visible
-  const album_id = props.album_id
+  const visible = props.visible;
+  const album_id = props.album_id;
 
-  function handleSubmit(){
-    if (!title || !date || !photo){
-        return alert("Preencha todos os dados")
+  function handleSubmit() {
+    if (!title || !date || !photo) {
+      return alert("Preencha todos os dados");
     }
-    if (!album_id){
-        return alert("Erro no site. Por favor reinicie a pagina e tente novamente")
+    if (!album_id) {
+      return alert(
+        "Erro no site. Por favor reinicie a pagina e tente novamente"
+      );
     }
-    api.post(`/pages/${album_id}`,{title,description,date})
-    .then(()=>{
-        alert("Pagina criada com sucesso")
+    api
+      .post(`/pages/${album_id}`, { title, description, date })
+      .then(() => {
+        alert("Pagina criada com sucesso");
         fetchAlbums();
       })
-      .catch(error=>{
-        if(error.response){
-          alert(error.response.data.message)
-        }else{
-          alert("Não foi possivel criar a pagina")
+      .catch((error) => {
+        if (error.response) {
+          alert(error.response.data.message);
+        } else {
+          alert("Não foi possivel criar a pagina");
         }
-      })
+      });
+  }
 
+  function handlePhotoChange(e) {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      // Use a regex to remove data url part
+      const base64String = reader.result
+        .replace("data:", "")
+        .replace(/^.+,/, "");
+
+      console.log(base64String);
+      setPhoto(base64String);
+
+    };
+    reader.readAsDataURL(file);
   }
 
   return (
@@ -66,7 +83,7 @@ export default function NewPageModal(props) {
               id="page-photo"
               type="file"
               value={photo}
-              onChange={(e) => setPhoto(e.target.files[0])}
+              onChange={handlePhotoChange}
             />
             <Button type="submit">Submit</Button>
           </form>
