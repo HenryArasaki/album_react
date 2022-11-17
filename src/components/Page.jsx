ca295a3d1d1f3fd63a262c8573ac091ae3d169e8
Henry Koiti Arasaki <82346140+HenryArasaki@users.noreply.github.com>
2022-11-17 18:48:42 -0300

    css page 3

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -4,13 +4,16 @@ export default function Page({ details, pageNumber, thisPage }) {
   return (
     <div
       className={`${
-        pageNumber == thisPage ? "block" : "hidden"
-      } p-4 h-180 flex-col flex justify-center`}
+        pageNumber == thisPage ? "block" : "hidden"}
+         p-4 h-180 flex-col flex justify-center items-center rounded-lg border border-black shadow-2xl`}
     >
       <h3 className="text-4xl text-center">{title}</h3>
       <p className="text-xl">{description}</p>
-      <img className="object-cover rounded-lg border border-black" src={photo} />
-      <span className="text-l text-right">{date}</span>
+      <img
+        className="rounded-lg shadow-lg h-auto md:w-3/5 2xl:w-2/5"
+        src={photo}
+      />
+      <span className="text-l text-right self-end mr-5">{date}</span>
     </div>
   );
 }
