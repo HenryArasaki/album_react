b4a1f7ef0b4bcc4afe0e09db88919635b47276d5
Henry Koiti Arasaki <82346140+HenryArasaki@users.noreply.github.com>
2022-11-17 14:58:41 -0300

    css page

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,12 +1,16 @@
+export default function Page({ details, pageNumber, thisPage }) {
+  const { title, description, date, photo } = details;
 
-
-export default function Page({details,pageNumber,thisPage}){
-    const {title,description,date,photo} = details
-
-    return(<div className={`${pageNumber == thisPage ?"block":"hidden"}`}>
-        <h3>{title}</h3>
-        <p>{description}</p>
-        <img src={photo} />
-        <span>{date}</span>
-</div>)
+  return (
+    <div
+      className={`${
+        pageNumber == thisPage ? "block" : "hidden"
+      } p-4 h-180 flex-col flex justify-center`}
+    >
+      <h3 className="text-4xl text-center">{title}</h3>
+      <p className="text-xl">{description}</p>
+      <img className="object-cover" src={photo} />
+      <span className="text-l text-right">{date}</span>
+    </div>
+  );
 }
